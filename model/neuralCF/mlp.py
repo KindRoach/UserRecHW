86d5f20265e44fa8a2d6c9447da637d3f2647439
kindroach <kindroach@hotmail.com>
2020-04-14 20:55:33 +0800

    Add the Multi-Layer Perception (MLP) model.

--- model/neuralCF/mlp.py
+++ model/neuralCF/mlp.py
@@ -2,28 +2,37 @@ from typing import List
 
 import torch
 
-from model.neuralCF.neural_helper import train_neural, TrainConfig
+from model.neuralCF.neural_helper import TrainConfig, train_neural
 from tool.data_reader import Rating
 
 
 def get_default_config() -> TrainConfig:
     return TrainConfig(num_epochs=100,
-                       batch_size=4096,
+                       batch_size=256,
                        learning_rate=0.01,
-                       l2_regularization=0.01,
+                       l2_regularization=0.0000001,
                        use_cuda=False)
 
 
-class GMF(torch.nn.Module):
+class MLP(torch.nn.Module):
+
     def __init__(self, num_users: int, num_items: int, latent_dim: int = 8,
+                 hidden_layer_dims: List[int] = [64, 32, 16],
                  train_config: TrainConfig = None):
-        super(GMF, self).__init__()
+        super(MLP, self).__init__()
 
         self.num_users = num_users + 1
         self.num_items = num_items + 1
         self.latent_dim = latent_dim
         self.embedding_user = torch.nn.Embedding(self.num_users, self.latent_dim)
         self.embedding_item = torch.nn.Embedding(self.num_items, self.latent_dim)
+
+        hidden_layer_dims.insert(0, latent_dim * 2)
+        hidden_layer_dims.append(latent_dim)
+        self.mlp_layer_dims = hidden_layer_dims
+        self.mlp_layers = torch.nn.ModuleList()
+        for in_size, out_size in zip(self.mlp_layer_dims[:-1], self.mlp_layer_dims[1:]):
+            self.mlp_layers.append(torch.nn.Linear(in_size, out_size))
 
         self.affine_output = torch.nn.Linear(self.latent_dim, 1)
         self.logistic = torch.nn.Sigmoid()
@@ -34,7 +43,7 @@ class GMF(torch.nn.Module):
         if train_config is not None:
             self.train_config = train_config
         else:
-            self.train_config = GMF.get_default_config()
+            self.train_config = MLP.get_default_config()
 
         if self.train_config.use_cuda:
             self.cuda()
@@ -42,8 +51,11 @@ class GMF(torch.nn.Module):
     def forward(self, user_indices, item_indices):
         user_embedding = self.embedding_user(user_indices)
         item_embedding = self.embedding_item(item_indices)
-        element_product = torch.mul(user_embedding, item_embedding)
-        logits = self.affine_output(element_product)
+        vector = torch.cat([user_embedding, item_embedding], dim=1)
+        for layer in self.mlp_layers:
+            vector = layer(vector)
+            vector = torch.nn.ReLU()(vector)
+        logits = self.affine_output(vector)
         rating = self.logistic(logits)
         return rating
 
--- train_neuralCF.py
+++ train_neuralCF.py
@@ -1,11 +1,13 @@
-from model.neuralCF import gmf
+from model.neuralCF import gmf, mlp
 from tool.data_reader import generate_implicit_ratings_with_negative, all_ratings, max_user_id, max_movie_id, \
     save_ratings, read_ratings
 
-# ratings = generate_implicit_ratings_with_negative(all_ratings, negative_num=4)
-# save_ratings(ratings, "data/ratings_with_negative.dat")
-ratings = read_ratings("data/ratings_with_negative.dat")
+ratings = generate_implicit_ratings_with_negative(all_ratings, negative_num=4)
+save_ratings(ratings, "data/ratings_with_negative.dat")
+# ratings = read_ratings("data/ratings_with_negative.dat")
+
 model = gmf.GMF(max_user_id, max_movie_id)
-config = gmf.default_config
-config.batch_size = 16 * 1024
+model.fit(ratings)
+
+model = mlp.MLP(max_user_id, max_movie_id)
 model.fit(ratings)
